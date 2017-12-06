0873e3b4ed221c1bc8fd52007eaa7328d186af8c
VikaSannikova <vikasannikova1998@gmail.com>
2017-12-06 13:54:35 +0300

    binary search

---
+++ AIAS_SannikovaVO_ShukinaAE/Example.cpp
@@ -0,0 +1,8 @@
+//using namespace std;
+//int pred(int i) {
+//	return i > 10;
+//}
+//int main(void) {
+//	int A[] = { 1,2,3,40,3,200,1 };
+//	vector
+//}
--- AIAS_SannikovaVO_ShukinaAE/SortArray.cpp
+++ AIAS_SannikovaVO_ShukinaAE/SortArray.cpp
@@ -5,6 +5,17 @@
 #include <algorithm>
 
 using namespace std;
+
+std::vector<int> vec = {1,2,3,4,5,7,8,9}; //втф
+int val = 1;
+int predicat(int i) {
+	if (val <= vec.front())
+		return vec.size();
+	if (val >= vec.back())
+		return 0;
+	return vec[i-1] > val;
+}
+
 int main() {
 	default_random_engine generator(time(0));
 	uniform_int_distribution <int> distribution(0, 1000);
@@ -13,6 +24,7 @@ int main() {
 	cin >> n;
 	vector<int> vector;
 	std::vector<int>::iterator it;
+
 
 	for (int i = 0; i < n; i++) {
 		vector.push_back(distribution(generator));
@@ -32,25 +44,57 @@ int main() {
 	auto start_time = std::chrono::steady_clock::now();
 	it = find(vector.begin(), vector.end(), tmp);
 	auto finish_time = std::chrono::steady_clock::now();
-		if (it!=vector.end()) {
-			cout << "element " << tmp << " found" << endl;
-		}
-		else {
-			cout << "element " << tmp << " NOT found" << endl;
-		}
+	if (it != vector.end()) {
+		cout << "element " << tmp << " found" << endl;
+	}
+	else {
+		cout << "element " << tmp << " NOT found" << endl;
+	}
 	auto result_time = std::chrono::duration_cast<std::chrono::microseconds>(finish_time - start_time);
 	cout << "find time: " << result_time.count() << endl;
-	
+
 
 	start_time = std::chrono::steady_clock::now();
-	vector.erase(find(vector.begin(), vector.end(), tmp));
+	vector.erase(find(vector.begin(), vector.end(), tmp)); //удаляет первый элемент
 	finish_time = std::chrono::steady_clock::now();
 	cout << "Array without tmp: " << endl;
 	for (it = vector.begin(); it != vector.end(); ++it) {
 		std::cout << *it << ' ';
 	}
 	result_time = std::chrono::duration_cast<std::chrono::microseconds>(finish_time - start_time);
-	cout << endl <<"Erase time: " << result_time.count() << endl;
+	cout << endl << "Erase time: " << result_time.count() << endl;
+
+
+
+
+	//start_time = std::chrono::steady_clock::now();
+	cout << "********************************" << endl << endl;
+	//cout<<vector.at( find            );
+
+
+
+
+
+	for (it = vec.begin(); it != vec.end(); ++it) {
+		std::cout << *it << ' ';
+	}
+	cout << endl;
+	vec.insert(find_if(vec.begin(), vec.end(), &predicat), val);
+	for (it = vec.begin(); it != vec.end(); ++it) {
+		std::cout << *it << ' ';
+	}
+
+
+	//for (it = vector.begin(); it != vector.end(); ++it) {
+	//	std::cout << *it << ' ';
+	//}
+	//cout << endl;
+	//int i = 0;
+	//vector.insert(find_if(vector.begin(), vector.end(), predicat(i,vector,tmp)) + 1, tmp);
+	//for (it = vector.begin(); it != vector.end(); ++it) {
+	//	std::cout << *it << ' ';
+	//}
+
 
 
 	system("pause");
